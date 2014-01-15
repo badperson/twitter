3cb0bb68d8880d534788a06f102ee3db21e03155
Rob Ashton <robashton@codeofrob.com>
2014-01-15 03:02:44 +0000

    Lolbots

--- heroku.js
+++
@@ -1,7 +0,0 @@
-var spawn = require('child_process').spawn
-  , config = require('./config')
-
-for(var k in config) {
-  spawn(['heroku config:set ', k, '=', config[k], '--app=badperson'].join(''))
-}
-
